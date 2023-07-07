cda542ce53b62d6e14bbc0f6f1f27ec71c1c3229
GordonGichini <gordongichini1@gmail.com>
2023-07-07 23:26:27 +0300

    basic detail form completed in dispatch form

--- frontend/mts/src/components/DispatchFormPage/index.js
+++ frontend/mts/src/components/DispatchFormPage/index.js
@@ -23,11 +23,31 @@ export default function DispatchFormPage() {
 
     const handleDriverPayChange = (event) => {
         setDriverPay(event.target.value);
+    }; 
+
+    const handleDriverAdvanceChange = (event) => {
+        setDriverAdvance(event.target.value);
     };
 
     const handleAddDriverClick = () => {
         // Perform logic for adding a driver
         // can access the captured input values here
+    };
+
+    const handleTruckChange = (event) => {
+        setTruck(event.target.value);
+    };
+
+    const handleTrailerChange = (event) => {
+        setTrailer(event.target.value);
+    };
+
+    const handleOdometerChange = (event) => {
+        setOdometer(event.target.value);
+    };
+
+    const handleCustomerChange = (event) => {
+        setCustomer(event.target.value);
     };
 
     const handleFormSubmit = (event) => {
@@ -50,7 +70,11 @@ export default function DispatchFormPage() {
             label="Driver Name"
             value={driverName}
             onChange={handleDriverNameChange}
-            />
+            /> 
+
+            <Button variant="contained" color="primary" onClick={handleDriverNameChange}>
+                Create Driver
+            </Button>
 
             <TextField
             label="Driver Pay"
@@ -58,9 +82,44 @@ export default function DispatchFormPage() {
             onChange={handleDriverPayChange}
             /> 
 
+            <TextField
+            label="Driver Advance"
+            value={driverAdvance}
+            onChange={handleDriverAdvanceChange}
+            />
+
             <Button variant="contained" color="primary" onClick={handleAddDriverClick}>
-                Add Driver
+                Add Team Driver
             </Button> 
+
+            <TextField
+            label="Truck"
+            value={truck}
+            onChange={handleTruckChange} 
+            /> 
+
+            <Button variant="contained" color="primary" onClick={handleTruckChange}>
+                Create Truck 
+                </Button>
+                <Button variant="contained" color="primary" onClick={handleTrailerChange}>
+                    Create Trailer
+                    </Button> 
+            
+            <TextField
+            label="Odometer"
+            value={odometer}
+            onChange={handleOdometerChange} 
+            />
+
+            <TextField
+            label="Customer"
+            value={Customer}
+            onChange={handleCustomerChange}
+            /> 
+
+            <Button variant="contained" color="primary" onClick={handleCustomerChange}>
+                Create customer
+            </Button>
 
             {/* Including other input fields and forms here */}
 
