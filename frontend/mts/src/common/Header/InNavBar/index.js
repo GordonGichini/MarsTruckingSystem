24ebb777a1d9b73061c4b6c21ac97d76b15d2e00
GordonGichini <gordongichini1@gmail.com>
2023-07-09 18:33:55 +0300

    expand more icon added, defined states in the innavbar

--- frontend/mts/src/common/Header/InNavBar/index.js
+++ frontend/mts/src/common/Header/InNavBar/index.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Button, makeStyles } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, IconButton, Button, makeStyles, Menu, MenuItem } from '@material-ui/core';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { Box, colors } from '@mui/material';
 import { NavLink, Link } from 'react-router-dom';
 import Divider from '@mui/material/Divider'; 
+import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -24,12 +25,33 @@ const useStyles = makeStyles((theme) => ({
   },
   active: {
     fontWeight: 'bold',
+  }, 
+  expandMoreIcon: {
+    marginLeft: theme.spacing(0.4)
   },
 }));
 
 
 export default function InNavBar() { 
   const classes = useStyles(); 
+  const [reportsAnchorEl, setReportsAnchorEl] = React.useState(null);
+  const [settingsAnchorEl, setSettingsAnchorEl] = React.useState(null);
+
+  const handleReportsMenuOpen = (event) => {
+    setReportsAnchorEl(event.currentTarget);
+  };
+
+  const handleReportsMenuClose = () => {
+    setReportsAnchorEl(null);
+  }; 
+
+  const handleSettingsMenuOpen = (event) => {
+    setSettingsAnchorEl(event.currentTarget);
+  };
+
+  const handleSettingsMenuClose = () => {
+    setSettingsAnchorEl(null);
+  };
   
   return (
     <AppBar position="static" className={classes.appBar}> 
@@ -103,7 +125,77 @@ export default function InNavBar() {
            exact
            activeClassName={classes.active}
            className={classes.navLink}
-          >Reports</Button>
+          >Reports</Button> 
+          <div>
+            <IconButton
+        edge="end"
+        color="inherit"
+        aria-label="reports"
+        aria-controls="reports-menu"
+        aria-haspopup="true"
+        onClick={handleReportsMenuOpen}
+      >
+        <ExpandMoreIcon />
+      </IconButton> 
+
+      <Menu
+        id="reports-menu"
+        anchorEl={reportsAnchorEl}
+        keepMounted
+        open={Boolean(reportsAnchorEl)}
+        onClose={handleReportsMenuClose}
+      >
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-company-overview">
+          Company Overview
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-profit-loss">
+          Profit and Loss
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-driver-pay">
+          Driver Settlement
+        </MenuItem> 
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-tax">
+          Tax
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-expenses">
+          Expenses
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-state-miles">
+          IRP - State Miles
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-fuel-reports">
+          Fuel Expenses
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-reefer-fuel-expenses">
+          Reefer Fuel Expenses
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-fuel-vendor-requests">
+          Fuel Vendor
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-ifta">
+          Quartely IFTA
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-quartely-maintenance">
+          Quartely Maintenance
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-unit-revenue-per-mile">
+          Unit Revenue
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-unit-operating-income">
+          Unit Operating Income
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-miles-per-gallon">
+          Miles Per Gallon
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-unit-miles">
+          Unit Miles
+        </MenuItem>
+        <MenuItem onClick={handleReportsMenuClose} component={NavLink} to="/reports/get-over-road-days">
+          Over the Road Days
+        </MenuItem>
+        </Menu>
+        </div>
+
           <Divider orientation="vertical" flexItem />
           <Button
            colour="inherit"
@@ -112,7 +204,44 @@ export default function InNavBar() {
            exact
            activeClassName={classes.active}
            className={classes.navLink}
-          >Settings</Button>
+          >Settings</Button> 
+          <div>
+      <IconButton
+        edge="end"
+        color="inherit"
+        aria-label="settings"
+        aria-controls="settings-menu"
+        aria-haspopup="true"
+        onClick={handleSettingsMenuOpen}
+      >
+        <ExpandMoreIcon />
+      </IconButton> 
+
+      <Menu
+        id="settings-menu"
+        anchorEl={settingsAnchorEl}
+        keepMounted
+        open={Boolean(settingsAnchorEl)}
+        onClose={handleSettingsMenuClose}
+      >
+        <MenuItem onClick={handleSettingsMenuClose} component={NavLink} to="/settings/addresses">
+          Addresses
+        </MenuItem>
+        <MenuItem onClick={handleSettingsMenuClose} component={NavLink} to="/settings/drivers">
+          Drivers
+        </MenuItem>
+        <MenuItem onClick={handleSettingsMenuClose} component={NavLink} to="/settings/units">
+          Units
+        </MenuItem>
+        <MenuItem onClick={handleSettingsMenuClose} component={NavLink} to="/settings/user-guide">
+          User Guide
+        </MenuItem> 
+        <MenuItem onClick={handleSettingsMenuClose} component={NavLink} to="/settings/forum">
+          Forum
+        </MenuItem>
+      </Menu>
+    </div>
+
           <Divider orientation="vertical" flexItem />
           <Button
            colour="inherit"
