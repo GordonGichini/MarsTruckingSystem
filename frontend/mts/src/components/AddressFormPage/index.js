072c334a5aab0085c734def5c459282407d4b8f6
GordonGichini <gordongichini1@gmail.com>
2023-08-02 23:27:13 +0300

    forms added

--- frontend/mts/src/components/AddressFormPage/index.js
+++ frontend/mts/src/components/AddressFormPage/index.js
@@ -1,36 +1,125 @@
 import React from 'react';
-import { Typography, TextField, Button, Box } from '@material-ui/core';
+import { Typography, TextField, Button, Box, Grid } from '@material-ui/core';
+import InNavBar from '../../common/Header/InNavBar';
+import { makeStyles } from '@mui/styles';
+import { useTheme } from '@mui/material/styles';
+
+const useStyles = makeStyles((theme) => ({
+  formContainer: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(4),
+    marginLeft: theme.spacing(4),
+    borderRadius: theme.spacing(2),
+  },
+  expenseContainer: {
+      marginTop: theme.spacing(2),
+      marginLeft: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+      padding: theme.spacing(2),
+
+  },
+  sectionTitle: {
+    marginBottom: theme.spacing(2),
+    marginTop: theme.spacing(10),
+    marginLeft: theme.spacing(2),
+  },
+  sectionSubtitle: {
+    marginBottom: theme.spacing(1),
+  },
+  search: {
+    width: '98%',
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(4),
+      marginLeft: theme.spacing(2),
+      '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'green',
+    },
+    '&:hover fieldset': {
+      borderColor: 'green',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'green',
+    },
+   },
+
+  },
+  inputField: {
+      width: '98%',
+      margin : theme.spacing(1, 0),
+      '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'green',
+    },
+    '&:hover fieldset': {
+      borderColor: 'green',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'green',
+    },
+   },
+    },
+  button: {
+      marginRight: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+      '& .MuiButton-outlined': {
+          borderColor: '#00C853', // Green color for button lines
+          color: '#00C853', // Green color for button text
+        },
+        '& .MuiButton-outlined:hover': {
+          backgroundColor: '#00C853', // Green color when hovered
+          color: '#FFFFFF', // White text color when hovered
+        },
+    },
+  }));
+
+
+
+
+
+
+
 
 export default function AddressFormPage() {
+  const classes = useStyles();
+  const theme = useTheme();
   const handleSaveClick = () => {
     // Logic for saving address details
   };
 
   return (
     <div>
-      <Typography variant="h6">Add Address</Typography>
+      <InNavBar />
+      <Typography variant="h6" className={classes.sectionTitle}>Add Address</Typography>
 
-      <Box my={2}>
-        <TextField label="Search" variant=""  />
+      <Box className={classes.formContainer}> 
+        <TextField label="Search" variant="outlined" className={classes.search} />
+        <Typography variant="caption">Select an address to autofill this form</Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+       
 
-        <TextField label="Company Name" variant="outlined" />
-        <TextField label="Street" variant="outlined" />
-        <TextField label="Apt/Suite/Other" variant="outlined" />
-        <TextField label="City" variant="outlined" />
-        <TextField label="State/Province" variant="outlined" />
-        <TextField label="ZIP Code" variant="outlined" />
-        <TextField label="Phone Number" variant="outlined" />
-        <TextField label="Phone Extension Number" variant="outlined" />
-        <TextField label="Alternate Phone" variant="outlined" />
-        <TextField label="Alternate Phone Extension" variant="outlined" />
-        <TextField label="Number" variant="outlined" />
-        <TextField label="Fax" variant="outlined" />
-        <TextField label="Email" variant="outlined" />
-        <TextField label="Website" variant="outlined" />
-        <TextField label="Contact" variant="outlined" />
-        <TextField label="Notes" variant="outlined" />
-        <TextField label="Motor Carrier Number" variant="outlined" />
-        <TextField label="Tax ID (EIN#)" variant="outlined" />
+        <TextField label="Company Name" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Street" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Apt/Suite/Other" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="City" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="State/Province" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="ZIP Code" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Phone Number" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Phone Extension Number" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Alternate Phone" margin='normal' variant="outlined" className={classes.inputField} />
+        </Grid>
+        <Grid item xs={6}>
+        <TextField label="Alternate Phone Extension" margin='normal' variant="outlined" className={classes.inputField} />
+        
+        <TextField label="Number" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Fax" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Email" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Website" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Contact" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Notes" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Motor Carrier Number" margin='normal' variant="outlined" className={classes.inputField} />
+        <TextField label="Tax ID (EIN#)" margin='normal' variant="outlined" className={classes.inputField} />
 
         <Button variant="contained" color="primary" onClick={handleSaveClick}>
           Save
@@ -38,6 +127,7 @@ export default function AddressFormPage() {
         <Button variant="text" color="secondary">
           Cancel
         </Button>
+        </Grid></Grid>
       </Box>
     </div>
   );
