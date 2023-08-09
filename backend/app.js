a14b06dc1ffbc83849574323ea0028fa33555566
GordonGichini <gordongichini1@gmail.com>
2023-08-10 01:33:26 +0300

    cors added

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const config = require('./config');
 const routes = require('./routes/routes');
 const errorMiddleware = require('./errorMiddleware');
@@ -16,12 +17,15 @@ mongoose.connect('mongodb+srv://gordongichini1:mUDzJ064sNgne1ss@cluster0.xm6xl0n
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 // Other middlewares and configurations
-
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 // Use the routes defined in routes.js for /api endpoints
 const protectedRoutes = require('./routes/routes');
 app.use('/api', protectedRoutes);
 
 app.use(routes);
+
 app.use(errorMiddleware);
 
 // Other routes and configurations
