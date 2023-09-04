009d5d6260f8cc96199873d3f3621d800fcf1912
GordonGichini <gordongichini1@gmail.com>
2023-09-04 19:37:40 +0300

    authentication feature and route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const config = require('../config');
 const jwt = require('jsonwebtoken');
 const { authenticateToken } = require('../authMiddleware');
 const User = require('../models/user');
 
-// Endpoint for user registration
-router.post('/register', async (req, res) => {
+
+exports.register = async (req, res) => {
   try {
       const { username, email, password } = req.body;
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = new User({ username, email, password: hashedPassword });
+
+      // Check if the email is already in use
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: 'Email already registered' });
+      }
+      const user = new User({ username, email, password });
       await user.save();
-      res.sendStatus(201);
-  } catch (err) {
-      console.error('Error while registering user:', err);
+      res.sendStatus(201).json({ message: 'Registration successful' });
+  } catch (error) {
+      console.error('Error while registering user:', error);
       res.sendStatus(500);
   }
-});
+};
 
-// Endpoint for user login
-router.post('/login', async (req, res) => {
+exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
@@ -32,18 +35,42 @@ router.post('/login', async (req, res) => {
     }
 
     // Check if the password is correct
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
+    const passwordMatch = await user.comparePassword(password);
+    if (!passwordMatch) {
       return res.status(401).json({ message: 'Invalid password.' });
     }
 
     // Generate a JWT token and send it in the response
     const token = jwt.sign({ id: user._id, email: user.email }, config.jwtSecret);
-    return res.status(200).json({ token });
+    return res.status(200).json({ token, message: 'Login successful' });
   } catch (error) {
     console.error('Error logging in:', error);
     return res.status(500).json({ message: 'Something went wrong.' });
   }
-});
+};
+
+//Logout a user
+exports.logout = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+
+    //checking if user exists
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    //Update the isLoggedIn status to false
+    user.isLoggedIn = false;
+    await user.save();
+
+    // Redirect or respond as needed (e.g clear cookies, destroy session)
+
+    res.status(200).json({ message: 'Logout successful' });
+  } catch (error) {
+    console.error('Error logging out:', error);
+    res.status(500).json({ message: 'Interval server error' });
+  }
+};
 
 module.exports = router;
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcyrpt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -15,7 +16,29 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  isLoggedIn: {
+    type: Boolean,
+    default: false,
+  },
+  // You can add more fields e.g role, company, etc.
 });
+
+// Lets Hash the password before saving it to the database
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcyrpt.genSalt(10);
+    const hashedPassword = await bcyrpt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Compare the provided password with the stored hashed password
+userSchema.methods.comparePassword = async function (password) {
+  return bcyrpt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
